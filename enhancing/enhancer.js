3c36e253e0df3915ae76dc0d3c82ef7a82a6edd4
David White <dwhite0627@gmail.com>
2020-05-06 19:33:09 -0400

    Add enhancer logic to enhancer.js

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,15 +6,28 @@ module.exports = {
 };
 
 function succeed(item) {
+  // Accepts an item object and returns a new item object
+  const maxEnhancement = 20;
+  item.enhancement += (item.enhancement < maxEnhancement ? 1 : 0);
   return { ...item };
 }
 
 function fail(item) {
+  // Accepts an item object and returns a new item object
+  let durabilityDecrease;
+  if (item.enhancement < 15) {
+    durabilitytDecrease = 5;
+  } else {
+    durabilityDecrease = 10;
+  }
+  item.durability -= durabilityDecrease;
+  item.enhancement -= (item.enhancement > 16 ? 1 : 0);
   return { ...item };
 }
 
 function repair(item) {
-  return { ...item };
+  // Accepts an item object and returns a new item with the durability restored to 100
+  return { ...item, durability: 100 };
 }
 
 function get(item) {
