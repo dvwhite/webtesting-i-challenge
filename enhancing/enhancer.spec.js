e90da20ec551d98daf518b1719085d3f9f840609
David White <dwhite0627@gmail.com>
2020-05-06 21:00:02 -0400

    Add verifyRanges helper function to check object properties are in the valid range

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -6,13 +6,22 @@ const range = end => {
   arr.unshift(0);
   return arr;
 };
+
 const verifyProperties = obj => {
   const keys = Object.keys(obj);
   const hasName = keys.includes("name");
   const hasDurability = keys.includes("durability");
   const hasEnhancement = keys.includes("enhancement");
   return (hasName && hasDurability && hasEnhancement);
-}
+};
+
+const verifyRanges = obj => {
+  const durabilityRange = range(100); // 0 to 100, inclusive
+  const enhanceRange = range(20); // 0 to 20, inclusive
+  const durabilityInRange = durabilityRange.includes(obj.durability);
+  const enhancementInRange = enhanceRange.includes(obj.enhancement);
+  return (durabilityInRange && enhancementInRange);
+};
 
 describe("item methods testing", () => {
   // Declare initial test vars
@@ -33,18 +42,20 @@ describe("item methods testing", () => {
     const newItem = enhancer.get(item1);
     expect(newItem).toEqual(item1); // Test equivalency
     expect(verifyProperties(newItem)).toBe(true); // Test it still has properties expected
+    delete newItem.durability;
+    expect(newItem).not.toEqual(item1);
   });
 
   // repair
   it("returns an object with durability=100 when repair(item) is called", () => {
-    // Normal case, expected usage, correct shape, no typos, no bugs in quantities
+    // Normal case, expected usage, correct shape, needs repairs, no bugs in quantities
     const repairedItem = enhancer.repair(item1);
     expect(repairedItem.durability).toBe(100);
     
-    // Unlikely cases - but nevertheless good to test for
+    // Unlikely cases - but nevertheless good to test for them
     
     //  Ensure it handles negative durability quantities
-    const buggyItem = { name: "Test 1t3m l", durability: -100, enhancement: 20 };
+    const buggyItem = { name: "Durability can't be negative", durability: -100, enhancement: 20 };
     const repairedBuggyItem = enhancer.repair(buggyItem);
     expect(repairedBuggyItem.durability).toBe(100);
 
